514c1edcfc4ad45e4ca525a642032b04f7724023
KiazUndaunted <kiaz.undaunted@gmail.com>
2020-02-15 15:42:39 -0500

    Added Scripts PlayerController PlayerMovement, Scene1, ground, cropped Emma image
    Added scripts PlayerController and PlayerMovement: now you can walk and jump.
    Added Scene1, added scripts to Emma sprite, added Rigidbody, Circle and Box colliders to Emma, added Box collider to ground.
    Added Ground placeholder.
    Changed Layer for Emma to Player, and layer for Ground to Ground.
    Cropped Emma image so there isn't huge white space around her.

---
+++ Emma/Assets/Emma/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Make slot to drag PlayerController script into
+    public PlayerController controller;
+
+    public float runSpeed = 40f;
+    float horizontalMovement = 0f;
+    bool jump = false;
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Get player input.
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        // Space to jump.
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Move character.
+        controller.Move(horizontalMovement * Time.fixedDeltaTime, jump);
+        // So you don't keep jumping forever:      
+        jump = false;
+    }
+}
