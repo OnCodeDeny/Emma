566ec77ff51b87192da59a7407afc9f6ba6813b3
OnCodeDeny <48635951+OnCodeDeny@users.noreply.github.com>
2020-02-19 15:41:09 -0500

    Add Player Interactionsn Mechanics
    Add Acorn Interaction, Leaf Interaction, Branch Interaction.

---
+++ Emma/Assets/Emma/Scripts/Acorn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acorn : MonoBehaviour
+{
+    public GameObject player;
+    public int amount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseUpAsButton()
+    {
+        player.SendMessage("GetAcorn", amount, SendMessageOptions.DontRequireReceiver);
+        Destroy(gameObject);
+    }
+}
