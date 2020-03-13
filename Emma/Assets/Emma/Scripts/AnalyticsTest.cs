1cac54ba4dc6e87d357f9f2d30e990f3f1df10a6
KiazUndaunted <48560897+KiazUndaunted@users.noreply.github.com>
2020-03-13 14:18:17 -0400

    Analytics Test
    Added AnalyticsTest script from class, Added empty game object, put script and general Analytics Event Tracker script to the empty game object, added button, made On Click event, dragged script into slot and changed the function to AnalyticsTest.ReportSecretFound. This means whenever the button is clicked, counts the event tracked from AnalyticsTest script.

---
+++ Emma/Assets/Emma/Scripts/AnalyticsTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class AnalyticsTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ReportSecretFound(0);
+        }
+    }
+
+    public void ReportSecretFound(int secretID)
+    {
+        print("event tracked");
+        Analytics.CustomEvent("secret_found", new Dictionary<string, object>
+    {
+        { "secret_id", secretID },
+        { "time_elapsed", Time.timeSinceLevelLoad }
+    });
+    }
+}
