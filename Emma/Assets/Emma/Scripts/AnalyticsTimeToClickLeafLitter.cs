527a3272c80143710a3e13b7357a001c59ad9f3a
KiazUndaunted <kiaz.undaunted@gmail.com>
2020-03-28 21:02:39 -0400

    Updated AnalyticsTimeToClickLeafLitter - comments
    Removed commented out code and commented remaining code

--- Emma/Assets/Emma/Scripts/AnalyticsTimeToClickLeafLitter.cs
+++ Emma/Assets/Emma/Scripts/AnalyticsTimeToClickLeafLitter.cs
@@ -4,66 +4,35 @@ using UnityEngine;
 using UnityEngine.Analytics;
 
 public class AnalyticsTimeToClickLeafLitter : MonoBehaviour
-{
-    //public static AcornMinigame AcornMinigameScript;
-    //public GameObject leafPile; 
+{    
     public EnterLeafMinigame EnterAcornMinigameScript;
     public bool ClickedLeafLitter;
-
-    //GameObject.Find("nameOfObjectYourScriptIsOn").GetComponent<move>().speed
-
-    //// In script other than move and also on different Game Object
-    //move _move = FindObjectOfType<move>();
-    //_move.speed = 10;
-
-//    public ScriptA script;
-
-//    void Start()
-//    {
-//        script = GameObject.FindObjectOfType<ScriptA>();
-//    }
-
-//    void Update()
-//    {
-//        if (script.variable...)
-//}
-
 
     // Start is called before the first frame update
     void Start()
     {
-        //EnterAcornMinigameScript = leafPile.GetComponent<EnterLeafMinigame>();
-        EnterAcornMinigameScript = GameObject.FindObjectOfType<EnterLeafMinigame>();
-      
-        
+        // get reference to script
+        EnterAcornMinigameScript = GameObject.FindObjectOfType<EnterLeafMinigame>();              
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        //desired: when click leaf litter, eg if bool == true
-        // maybe put in Tyler's script, and access here:
-        // if (EnterLeafMinigame.ClickedLeafLitterBool == true)
-
-        //GameObject.Find("nameOfObjectYourScriptIsOn").GetComponent<move>().speed
+        // Find the boolean in the script EnterLeafMinigame on the gameobject Leaf Pile
         ClickedLeafLitter = GameObject.Find("Leaf Pile").GetComponent<EnterLeafMinigame>().ClickedLeafLitterBool;
-        Debug.Log("In AnalyticsTimeToClickLeafLitter bool ClickedLeafLitter= " + ClickedLeafLitter);
+        //Debug.Log("In AnalyticsTimeToClickLeafLitter bool ClickedLeafLitter= " + ClickedLeafLitter);
+
+        // if that boolean is true, call the function ReportLeafLitterClicked
         if (ClickedLeafLitter == true)
         {
-            Debug.Log("ClickedLeafLitter in AnalyticsTimeToClickLeafLitter");
+            //Debug.Log("ClickedLeafLitter in AnalyticsTimeToClickLeafLitter");
             ReportLeafLitterClicked(0);
         }
-
-        //test
-        //if (Input.GetKeyDown(KeyCode.A))
-        //{
-        //    ReportLeafLitterClicked(0);
-        //}
     }
 
     public void ReportLeafLitterClicked(int leafLitterClickedID)
     {
+        // track the event for analytics, which you can find through "go to dashboard" in Services in Unity
         print("ClickLeafLitterEvent tracked");
         Analytics.CustomEvent("LeafLitterEvent_Clicked", new Dictionary<string, object>
         {
